7c71000c09f759534b0e8bad4166ca3eb8ca4036
Joseph M. Koning <koning1@llnl.gov>
2019-12-04 16:50:07 -0800

    Move develop to the front so maestro will install.

--- var/spack/repos/builtin/packages/py-maestrowf/package.py
+++ var/spack/repos/builtin/packages/py-maestrowf/package.py
@@ -16,12 +16,12 @@ class PyMaestrowf(PythonPackage):
 
     maintainers = ['FrankD412']
 
+    version("develop", branch="develop")
     version('1.1.4', sha256='2cb0fa6f6281d8618ac79217ea5f4fd8cb24955c4315e873657f96b815f171d5')
     version('1.1.2', sha256='ebb45bff54625435bc9f2462e1bdc3b5bdc4d943378c53e7810c11836794c5e0')
     version('1.1.1', sha256='a476ad4b40846d7b7f9540d6413df1b42eb655735e8d3c6c07e0baa68e20a8bb')
     version('1.1.0', sha256='14e701d6a10ab758215aab6b6809817d9a39416a4f477cd2f2551883fc68477b')
     version('1.0.1', sha256='cdd503f0b11db9114405132274b28766044402d1183b5836406ed91d558fd06c')
-    version("develop", branch="develop")
 
     depends_on('py-setuptools', type='build')
     depends_on('py-pyyaml@4.2b1:',     type=('build', 'run'))
